810bc048fafb85c88260635c34191b777dc64bd5
HaroonBadshah <haroonbadshah04@gmail.com>
2021-07-03 14:24:28 +0500

    test file created

---
+++ lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'model/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'C1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'C2',
+    title: 'PizzaHut',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'C3',
+    title: 'CheifBurger',
+    color: Colors.lightBlueAccent,
+  ),
+  Category(
+    id: 'C4',
+    title: 'QuickandEasy',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'C5',
+    title: 'fruits',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'C6',
+    title: 'Spagatte',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'C7',
+    title: 'French',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'C8',
+    title: 'Summer',
+    color: Colors.redAccent,
+  ),
+  Category(
+    id: 'C9',
+    title: 'Chines',
+    color: Colors.yellowAccent,
+  ),
+  Category(
+    id: 'C10',
+    title: 'Turkish',
+    color: Colors.grey,
+  ),
+];
